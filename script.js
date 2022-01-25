114dac836becdb7e00958053f5541e56a4bb7d46
luaroxy <luannamsiqueira@gmail.com>
2022-01-24 22:04:14 -0600

    Set up hover effect

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ for (let i=0; i<(16*16); i++){
     square.classList.add("cell");
     document.getElementById("grid").appendChild(square);
 }
+
+let cells = document.querySelectorAll(".cell");
+
+cells.forEach((div) => {
+div.addEventListener('mouseover', function(){
+  div.style.background = "pink"
+});
+});
