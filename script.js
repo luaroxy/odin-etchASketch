7ef5c2640ce742dd1f29442ae7d4d6cc44d7b1d0
luaroxy <luannamsiqueira@gmail.com>
2022-01-25 22:13:37 -0600

    Added clear button and started prompt

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+numberOfSquares = 16;
+console.log(numberOfSquares);
 
-for (let i=0; i<(16*16); i++){
+const button = document.querySelector('button');
+button.addEventListener('click',() => {
+  cells.forEach((div) => {
+      div.style.background = ""
+    });
+  
+    let numberOfSquares = window.prompt("Please enter the number of squares per side between 1 and 100");
+    console.log(numberOfSquares);
+})
+
+function createSquares(numberOfSquares){
+  for (let i=0; i<(numberOfSquares*numberOfSquares); i++){
     const square = document.createElement("div");
     square.classList.add("cell");
     document.getElementById("grid").appendChild(square);
+  }
 }
 
-let cells = document.querySelectorAll(".cell");
+createSquares(numberOfSquares);
 
+let cells = document.querySelectorAll(".cell");
 cells.forEach((div) => {
 div.addEventListener('mouseover', function(){
-  div.style.background = "pink"
+div.style.background = "pink"
 });
 });
