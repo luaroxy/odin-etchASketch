ee88a4002edc57520f58a0087846bc26ab3d49f3
luaroxy <luannamsiqueira@gmail.com>
2022-01-30 20:49:14 -0600

    Project completed

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 numberOfSquares = 16;
-console.log(numberOfSquares);
 
 const button = document.querySelector('button');
 button.addEventListener('click',() => {
+  let cells = document.querySelectorAll(".cell");
   cells.forEach((div) => {
       div.style.background = ""
     });
   
     let numberOfSquares = window.prompt("Please enter the number of squares per side between 1 and 100");
-    console.log(numberOfSquares);
+    if (numberOfSquares>=1 && numberOfSquares<=100){
+      deleteSquares();
+      start(numberOfSquares);
+    }
+    else window.alert("The number of squares per side must be between 1 and 100");
 })
 
 function createSquares(numberOfSquares){
@@ -19,11 +23,27 @@ function createSquares(numberOfSquares){
   }
 }
 
-createSquares(numberOfSquares);
+function deleteSquares(){
+  while (document.getElementById("grid").firstChild) {
+    document.getElementById("grid").removeChild(document.getElementById("grid").lastChild);
+  }
+}
 
-let cells = document.querySelectorAll(".cell");
-cells.forEach((div) => {
-div.addEventListener('mouseover', function(){
-div.style.background = "pink"
-});
-});
+function start(numberOfSquares){
+  createSquares(numberOfSquares);
+  document.documentElement.style.setProperty("--squareNum", numberOfSquares);
+
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach((div) => {
+  div.addEventListener('mouseover', function(){
+  div.style.background = random_rgb();
+  });
+  });
+}
+
+function random_rgb() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
+}
+
+start(numberOfSquares);
